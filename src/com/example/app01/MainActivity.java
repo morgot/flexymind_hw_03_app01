d989f301046014e038846f1b44f00b65138e51f0
Gregory <morgot.gregory@gmail.com>
2013-04-06 16:35:19 +0400

    Добавил layout, обеспечил переход посредством кнопок.

--- src/com/example/app01/MainActivity.java
+++ src/com/example/app01/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.app01;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.content.Intent;
+
 
 public class MainActivity extends Activity {
 
@@ -18,5 +21,13 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	// Переход на страницу регистрации
+	
+	public void goToRegistrationActivity( View view){
+		Intent intent = new Intent ( this, RegistrationActivity.class);
+		startActivity(intent);
+	}
+	 public void finishMainActivity(View v) {
+	        MainActivity.this.finish();
+	    }
 }
